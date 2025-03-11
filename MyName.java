23e85ba3b265a0267157b96852f8eeeecbe77ee9
Shahnawaz <sa522372@gmail.com>
2025-03-11 09:57:24 +0530

    My First Program

---
+++ MyName.java
@@ -0,0 +1,12 @@
+// Automation Batch 51 Assignment 1; January 18th 2025
+// WAP to print your name.
+package package001;
+
+public class MyName {
+	public static void main(String[] args) {
+		String name = "Md Shahnawaz Ansari";
+		System.out.println( name);
+		
+	}
+
+}
